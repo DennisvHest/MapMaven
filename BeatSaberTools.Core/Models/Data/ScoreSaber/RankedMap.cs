b71bf460182e4ad05518d7473ffc77a56a1f43cb
Dennis van Hest <dennisvhest@hotmail.nl>
2022-11-06 21:01:43 +0100

    PP estimate fixed?

--- BeatSaberTools.Core/Models/Data/ScoreSaber/RankedMap.cs
+++ BeatSaberTools.Core/Models/Data/ScoreSaber/RankedMap.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿
+using System.Text.Json.Serialization;
 
 namespace BeatSaberTools.Core.Models.Data.ScoreSaber
 {
@@ -10,12 +11,12 @@ namespace BeatSaberTools.Core.Models.Data.ScoreSaber
         public string Artist { get; set; }
         public string Mapper { get; set; }
         public decimal Bpm { get; set; }
-        [JsonProperty("diff")]
+        [JsonPropertyName("diff")]
         public string Difficulty { get; set; }
         public int Scores { get; set; }
         public decimal Stars { get; set; }
         public decimal PP { get; set; }
-        [JsonProperty("beatSaverKey")]
+        [JsonPropertyName("beatSaverKey")]
         public string Key { get; set; }
         public int Downloads { get; set; }
         public int Upvotes { get; set; }
@@ -24,7 +25,7 @@ namespace BeatSaberTools.Core.Models.Data.ScoreSaber
         public string Download { get; set; }
         public decimal Duration { get; set; }
         public int NoteCount { get; set; }
-        [JsonProperty("njs")]
+        [JsonPropertyName("njs")]
         public decimal NoteJumpSpeed { get; set; }
         public int RecentScores { get; set; }
     }
