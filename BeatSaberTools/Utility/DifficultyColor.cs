994dd95aa0718d98b03a14a548124aa0b5886526
Dennis van Hest <dennisvhest@hotmail.nl>
2022-12-24 21:55:53 +0100

    Improvement grid styling

---
+++ BeatSaberTools/Utility/DifficultyColor.cs
@@ -0,0 +1,20 @@
+﻿using Colors = MudBlazor.Colors;
+
+namespace BeatSaberTools.Utility
+{
+    public static class DifficultyColor
+    {
+        public static string Get(string difficulty)
+        {
+            return difficulty switch
+            {
+                "ExpertPlus" => Colors.Purple.Darken3,
+                "Expert" => Colors.Red.Darken3,
+                "Hard" => Colors.Orange.Darken3,
+                "Normal" => Colors.Blue.Darken3,
+                "Easy" => Colors.Green.Darken3,
+                _ => Colors.Shades.Black
+            };
+        }
+    }
+}
