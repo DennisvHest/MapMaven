17c6adcc2e6288d5bfbf4cd8f37f60ea62e50597
Dennis van Hest <dennisvhest@hotmail.nl>
2023-02-26 15:10:17 +0100

    Application launches only once

--- MapMaven/Platforms/Windows/App.xaml.cs
+++ MapMaven/Platforms/Windows/App.xaml.cs
@@ -1,6 +1,11 @@
 ﻿// To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
+using Hardcodet.Wpf.TaskbarNotification.Interop;
+using MapMaven.Platforms.Windows;
+using Microsoft.UI.Xaml;
+using System.Diagnostics;
+
 namespace MapMaven.WinUI;
 
 /// <summary>
@@ -8,15 +13,40 @@ namespace MapMaven.WinUI;
 /// </summary>
 public partial class App : MauiWinUIApplication
 {
-	/// <summary>
-	/// Initializes the singleton application object.  This is the first line of authored code
-	/// executed, and as such is the logical equivalent of main() or WinMain().
-	/// </summary>
-	public App()
+    private readonly SingleInstanceDesktopApp _singleInstanceApp;
+
+    private LaunchActivatedEventArgs _launchEventArgs = null;
+
+    /// <summary>
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
+    public App()
 	{
-		this.InitializeComponent();
-	}
+		InitializeComponent();
+
+        _singleInstanceApp = new SingleInstanceDesktopApp("MAP-MAVEN");
+        _singleInstanceApp.Launched += OnSingleInstanceLaunched;
+    }
 
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        _launchEventArgs = args;
+        _singleInstanceApp.Launch(args.Arguments);
+    }
+
+    private void OnSingleInstanceLaunched(object? sender, SingleInstanceLaunchEventArgs e)
+    {
+        if (e.IsFirstLaunch)
+        {
+            base.OnLaunched(_launchEventArgs);
+        }
+        else
+        {
+            Platforms.Windows.WindowExtensions.BringToFront();
+        }
+    }
 }
 
