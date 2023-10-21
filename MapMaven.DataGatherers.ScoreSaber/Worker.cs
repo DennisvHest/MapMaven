e16aabb0228cd20f2a2e68e51ad65cb7effda3f0
Dennis van Hest <dennisvhest@hotmail.nl>
2023-10-21 21:00:37 +0200

    Fetching players in ScoreSaber data gatherer

--- MapMaven.DataGatherers.ScoreSaber/Worker.cs
+++ MapMaven.DataGatherers.ScoreSaber/Worker.cs
@@ -1,24 +1,86 @@
+using ComposableAsync;
 using MapMaven.DataGatherers.ScoreSaber.Data;
+using MapMaven.DataGatherers.Shared;
 using Microsoft.EntityFrameworkCore;
+using RateLimiter;
 
 namespace MapMaven.DataGatherers.ScoreSaber
 {
     public class Worker : BackgroundService
     {
-        private readonly ScoreSaberApiClient scoreSaberApiClient;
-        private readonly ScoreSaberScoresContext db;
+        private readonly ScoreSaberApiClient _scoreSaber;
+        private readonly ScoreSaberScoresContext _db;
+
+        private readonly TimeLimiter _scoreSaberRateLimit = TimeLimiter.GetFromMaxCountByInterval(380, TimeSpan.FromMinutes(1));
+        private readonly SemaphoreSlim _dbSemaphore = new SemaphoreSlim(1, 1);
 
         private readonly ILogger<Worker> _logger;
 
-        public Worker(ILogger<Worker> logger, ScoreSaberScoresContext db)
+        public Worker(ILogger<Worker> logger, ScoreSaberScoresContext db, ScoreSaberApiClient scoreSaber)
         {
             _logger = logger;
-            this.db = db;
+            _db = db;
+            _db.ChangeTracker.AutoDetectChangesEnabled = false;
+            _scoreSaber = scoreSaber;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await db.Database.MigrateAsync();
+            await _db.Database.MigrateAsync();
+
+            await GetAllPlayersAsync(stoppingToken);
+        }
+
+        private async Task GetAllPlayersAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Fetching all players...");
+
+            var existingPlayerIds = _db.Players.Select(x => x.Id).ToHashSet();
+
+            var firstPlayersPage = await _scoreSaber.PlayersAsync(
+                search: string.Empty,
+                page: 1,
+                countries: string.Empty,
+                withMetadata: true,
+                cancellationToken: cancellationToken
+            );
+
+            using var progressReporter = new ProgressReporter(firstPlayersPage.Metadata.Total, (int)firstPlayersPage.Metadata.ItemsPerPage, _logger);
+
+            var totalPages = (int)Math.Ceiling(firstPlayersPage.Metadata.Total / firstPlayersPage.Metadata.ItemsPerPage);
+
+            var playerRequests = Enumerable.Range(1, totalPages).Select(async page =>
+            {
+                await _scoreSaberRateLimit;
+
+                var players = await _scoreSaber.PlayersAsync(
+                    search: string.Empty,
+                    page: page,
+                    countries: string.Empty,
+                    withMetadata: false
+                );
+
+                if (!players.Players.Any(x => existingPlayerIds.Contains(x.Id)))
+                {
+                    await _dbSemaphore.WaitAsync();
+
+                    try
+                    {
+                        _db.Players.AddRange(players.Players);
+                        await _db.SaveChangesAsync(cancellationToken);
+                    }
+                    finally
+                    {
+                        _dbSemaphore.Release();
+                    }
+                }
+
+                progressReporter.ReportProgress();
+            });
+
+            await Task.WhenAll(playerRequests);
+
+            _logger.LogInformation("Done fetching all players.");
         }
     }
 }
---
+++ MapMaven.DataGatherers.Shared/ProgressReporter.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace MapMaven.DataGatherers.Shared
+{
+    public class ProgressReporter : IDisposable
+    {
+        private Stopwatch _stopwatch;
+
+        private ILogger _logger;
+
+        private int _totalItems;
+        private int _itemsPerPage;
+        private int _completedPages = 0;
+
+        public ProgressReporter(int totalItems, int itemsPerPage, ILogger logger)
+        {
+            _stopwatch = Stopwatch.StartNew();
+
+            _totalItems = totalItems;
+            _itemsPerPage = itemsPerPage;
+            _logger = logger;
+        }
+
+        public void ReportProgress()
+        {
+            _completedPages++;
+
+            var totalPages = _totalItems / _itemsPerPage;
+
+            var averageTime = _stopwatch.Elapsed / _completedPages;
+            var estimatedTimeLeft = averageTime * (totalPages - _completedPages);
+
+            _logger.LogInformation($"Fetched: {_completedPages}/{totalPages} ({(double)_completedPages / totalPages:#0.##%}). Average request duration: {averageTime}. Elapsed time: {_stopwatch.Elapsed} Estimated time left: {estimatedTimeLeft}");
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+        }
+    }
+}
