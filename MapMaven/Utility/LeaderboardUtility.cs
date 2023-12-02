795bb8bc1219735fe800b5144aa8b60bd15b5377
Dennis van Hest <dennisvhest@hotmail.nl>
2023-12-02 21:08:08 +0100

    Showing leaderboard provider icon in playlist name in map browser

---
+++ MapMaven/Utility/LeaderboardUtility.cs
@@ -0,0 +1,17 @@
+﻿using MapMaven.Core.Models;
+
+namespace MapMaven.Utility
+{
+    public static class LeaderboardUtility
+    {
+        public static string GetLogoPath(LeaderboardProvider leaderboardProvider)
+        {
+            return leaderboardProvider switch
+            {
+                LeaderboardProvider.ScoreSaber => "/images/score-saber-logo-small.png",
+                LeaderboardProvider.BeatLeader => "/images/beat-leader-logo.svg",
+                _ => throw new NotImplementedException(),
+            };
+        }
+    }
+}
