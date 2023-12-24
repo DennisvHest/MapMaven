0106ce5aece90f585ef790a1fb4e602f6137b24a
Dennis van Hest <dennisvhest@hotmail.nl>
2023-12-24 22:08:25 +0100

    Dependency injection setup for xunit tests + setup in memory cache db for tests

---
+++ MapMaven.Core.Tests/MapMavenTestBedFixture.cs
@@ -0,0 +1,34 @@
+﻿using MapMaven.Infrastructure;
+using MapMaven.Infrastructure.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Xunit.Microsoft.DependencyInjection;
+using Xunit.Microsoft.DependencyInjection.Abstracts;
+
+namespace MapMaven.Core.Tests
+{
+    public class MapMavenTestBedFixture : TestBedFixture
+    {
+        private SqliteConnection _dbConnection;
+
+        protected override void AddServices(IServiceCollection services, IConfiguration? configuration)
+        {
+            services.AddMapMaven(useStatefulServices: true);
+
+            _dbConnection = new SqliteConnection("DataSource=:memory:");
+            _dbConnection.Open();
+
+            services.RemoveAll<DbContextOptions<MapMavenContext>>();
+            services.AddDbContext<MapMavenContext>(options =>
+                options.UseSqlite(_dbConnection)
+            );
+        }
+
+        protected override async ValueTask DisposeAsyncCore() => await _dbConnection.DisposeAsync();
+
+        protected override IEnumerable<TestAppSettings> GetTestAppSettings() => Enumerable.Empty<TestAppSettings>();
+    }
+}
---
+++ MapMaven.Core.Tests/SetupTest.cs
@@ -0,0 +1,40 @@
+﻿using MapMaven.Core.Services.Interfaces;
+using MapMaven.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reactive.Linq;
+using Xunit.Abstractions;
+using Xunit.Microsoft.DependencyInjection.Abstracts;
+
+namespace MapMaven.Core.Tests
+{
+    [CollectionDefinition("Map Maven DI")]
+    public class SetupTest : TestBed<MapMavenTestBedFixture>, IAsyncLifetime
+    {
+        private readonly MapMavenContext _db;
+        private readonly IApplicationSettingService _applicationSettingService;
+        private readonly IMapService _mapService;
+
+
+        public SetupTest(ITestOutputHelper testOutputHelper, MapMavenTestBedFixture fixture) : base(testOutputHelper, fixture)
+        {
+            _db = _fixture.GetService<MapMavenContext>(_testOutputHelper)!;
+            _applicationSettingService = _fixture.GetService<IApplicationSettingService>(_testOutputHelper)!;
+            _mapService = _fixture.GetService<IMapService>(_testOutputHelper)!;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _db.Database.MigrateAsync();
+            await _applicationSettingService.LoadAsync();
+            await _mapService.RefreshDataAsync();
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            var maps = await _mapService.Maps.FirstAsync();
+        }
+
+        Task IAsyncLifetime.DisposeAsync() => Task.CompletedTask;
+    }
+}
