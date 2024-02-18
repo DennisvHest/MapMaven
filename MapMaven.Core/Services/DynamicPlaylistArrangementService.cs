4f4e6a3ff33d082f0ec024e908c9fbe161aa9e88
Dennis van Hest <dennisvhest@hotmail.nl>
2024-02-18 22:31:11 +0100

    Working advanced filters

--- MapMaven.Core/Services/DynamicPlaylistArrangementService.cs
+++ MapMaven.Core/Services/DynamicPlaylistArrangementService.cs
@@ -173,85 +173,10 @@ namespace MapMaven.Core.Services
         {
             foreach (var filterOperation in configuration.FilterOperations)
             {
-                maps = maps.Where(map => FilterOperationMatches(map, filterOperation));
+                maps = maps.Where(map => MapSearchService.FilterOperationMatches(map.DynamicPlaylistMap, filterOperation));
             }
 
             return maps;
-        }
-
-        private bool FilterOperationMatches(DynamicPlaylistMapPair mapPair, FilterOperation filterOperation)
-        {
-            var map = mapPair.DynamicPlaylistMap;
-
-            var value = _resolver.ResolveSafe(map, filterOperation.Field);
-
-            if (value is string stringValue)
-            {
-                return filterOperation.Operator switch
-                {
-                    FilterOperator.Equals => stringValue.Equals(filterOperation.Value, StringComparison.OrdinalIgnoreCase),
-                    FilterOperator.NotEquals => !stringValue.Equals(filterOperation.Value, StringComparison.OrdinalIgnoreCase),
-                    FilterOperator.Contains => stringValue.Contains(filterOperation.Value, StringComparison.OrdinalIgnoreCase),
-                    FilterOperator.NotContains => !stringValue.Contains(filterOperation.Value, StringComparison.OrdinalIgnoreCase),
-                    _ => false
-                };
-            }
-
-            if (value is double doubleValue)
-            {
-                var compareValue = double.Parse(filterOperation.Value, CultureInfo.InvariantCulture);
-
-                return filterOperation.Operator switch
-                {
-                    FilterOperator.Equals => doubleValue == compareValue,
-                    FilterOperator.NotEquals => doubleValue != compareValue,
-                    FilterOperator.GreaterThan => doubleValue > compareValue,
-                    FilterOperator.LessThan => doubleValue < compareValue,
-                    FilterOperator.LessThanOrEqual => doubleValue <= compareValue,
-                    FilterOperator.GreaterThanOrEqual => doubleValue >= compareValue,
-                    _ => false
-                };
-            }
-
-            if (value is DateTime dateTimeValue)
-            {
-                var compareValue = DateTime.Parse(filterOperation.Value, CultureInfo.InvariantCulture);
-
-                return filterOperation.Operator switch
-                {
-                    FilterOperator.Equals => dateTimeValue == compareValue,
-                    FilterOperator.NotEquals => dateTimeValue != compareValue,
-                    FilterOperator.GreaterThan => dateTimeValue > compareValue,
-                    FilterOperator.LessThan => dateTimeValue < compareValue,
-                    FilterOperator.LessThanOrEqual => dateTimeValue <= compareValue,
-                    FilterOperator.GreaterThanOrEqual => dateTimeValue >= compareValue,
-                    _ => false
-                };
-            }
-
-            if (value is bool boolValue)
-            {
-                var compareValue = bool.Parse(filterOperation.Value);
-
-                return filterOperation.Operator switch
-                {
-                    FilterOperator.Equals => boolValue == compareValue,
-                    FilterOperator.NotEquals => boolValue != compareValue,
-                    _ => false
-                };
-            }
-
-            if (value is IEnumerable<string> stringEnumerableValue)
-            {
-                return filterOperation.Operator switch
-                {
-                    FilterOperator.Contains => stringEnumerableValue.Contains(filterOperation.Value, StringComparer.OrdinalIgnoreCase),
-                    FilterOperator.NotContains => !stringEnumerableValue.Contains(filterOperation.Value, StringComparer.OrdinalIgnoreCase),
-                    _ => false
-                };
-            }
-
-            return false;
         }
 
         private IEnumerable<DynamicPlaylistMapPair> SortMaps(IEnumerable<DynamicPlaylistMapPair> maps, DynamicPlaylistConfiguration configuration)
---
+++ MapMaven.Core/Services/MapSearchService.cs
@@ -0,0 +1,86 @@
+﻿using MapMaven.Core.Models.AdvancedSearch;
+using MapMaven.Core.Models.DynamicPlaylists;
+using MapMaven.Core.Models.DynamicPlaylists.MapInfo;
+using Pather.CSharp;
+using System.Globalization;
+
+namespace MapMaven.Core.Services
+{
+    public class MapSearchService
+    {
+        private static readonly IResolver _resolver = new Resolver();
+
+        public static bool FilterOperationMatches(DynamicPlaylistMap map, FilterOperation filterOperation)
+        {
+            var value = _resolver.ResolveSafe(map, filterOperation.Field);
+
+            if (value is string stringValue)
+            {
+                return filterOperation.Operator switch
+                {
+                    FilterOperator.Equals => stringValue.Equals(filterOperation.Value, StringComparison.OrdinalIgnoreCase),
+                    FilterOperator.NotEquals => !stringValue.Equals(filterOperation.Value, StringComparison.OrdinalIgnoreCase),
+                    FilterOperator.Contains => stringValue.Contains(filterOperation.Value, StringComparison.OrdinalIgnoreCase),
+                    FilterOperator.NotContains => !stringValue.Contains(filterOperation.Value, StringComparison.OrdinalIgnoreCase),
+                    _ => false
+                };
+            }
+
+            if (value is double doubleValue)
+            {
+                var compareValue = double.Parse(filterOperation.Value, CultureInfo.InvariantCulture);
+
+                return filterOperation.Operator switch
+                {
+                    FilterOperator.Equals => doubleValue == compareValue,
+                    FilterOperator.NotEquals => doubleValue != compareValue,
+                    FilterOperator.GreaterThan => doubleValue > compareValue,
+                    FilterOperator.LessThan => doubleValue < compareValue,
+                    FilterOperator.LessThanOrEqual => doubleValue <= compareValue,
+                    FilterOperator.GreaterThanOrEqual => doubleValue >= compareValue,
+                    _ => false
+                };
+            }
+
+            if (value is DateTime dateTimeValue)
+            {
+                var compareValue = DateTime.Parse(filterOperation.Value, CultureInfo.InvariantCulture);
+
+                return filterOperation.Operator switch
+                {
+                    FilterOperator.Equals => dateTimeValue == compareValue,
+                    FilterOperator.NotEquals => dateTimeValue != compareValue,
+                    FilterOperator.GreaterThan => dateTimeValue > compareValue,
+                    FilterOperator.LessThan => dateTimeValue < compareValue,
+                    FilterOperator.LessThanOrEqual => dateTimeValue <= compareValue,
+                    FilterOperator.GreaterThanOrEqual => dateTimeValue >= compareValue,
+                    _ => false
+                };
+            }
+
+            if (value is bool boolValue)
+            {
+                var compareValue = bool.Parse(filterOperation.Value);
+
+                return filterOperation.Operator switch
+                {
+                    FilterOperator.Equals => boolValue == compareValue,
+                    FilterOperator.NotEquals => boolValue != compareValue,
+                    _ => false
+                };
+            }
+
+            if (value is IEnumerable<string> stringEnumerableValue)
+            {
+                return filterOperation.Operator switch
+                {
+                    FilterOperator.Contains => stringEnumerableValue.Contains(filterOperation.Value, StringComparer.OrdinalIgnoreCase),
+                    FilterOperator.NotContains => !stringEnumerableValue.Contains(filterOperation.Value, StringComparer.OrdinalIgnoreCase),
+                    _ => false
+                };
+            }
+
+            return false;
+        }
+    }
+}
