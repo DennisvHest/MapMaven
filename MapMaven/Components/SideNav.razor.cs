4d176703e2d4a48165a43fb6df910f5b9dc139b9
Dennis van Hest <dennisvhest@hotmail.nl>
2024-08-17 21:43:31 +0200

    Saving sidenav width in localstorage

--- MapMaven/Components/SideNav.razor.cs
+++ MapMaven/Components/SideNav.razor.cs
@@ -1,6 +1,7 @@
 using MapMaven.Core.Services.Interfaces;
 using MapMaven.Core.Services.Leaderboards;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using MudBlazor;
 
 namespace MapMaven.Components
@@ -8,19 +9,28 @@ namespace MapMaven.Components
     public partial class SideNav
     {
         [Inject]
-        protected IPlaylistService PlaylistService { get; set; }
+        IPlaylistService PlaylistService { get; set; }
 
         [Inject]
         ILeaderboardService ScoreSaberService { get; set; }
 
         [Inject]
-        public IDialogService DialogService { get; set; }
+        IDialogService DialogService { get; set; }
+
+        [Inject]
+        IJSRuntime JS { get; set; }
 
         public string? PlayerId { get; set; } = null;
 
         protected override void OnInitialized()
         {
             SubscribeAndBind(ScoreSaberService.PlayerIdObservable, playerId => PlayerId = playerId);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+                await JS.InvokeVoidAsync("window.sideNavResizer.initialize");
         }
 
         protected void OnMapsNavigation()
--- MapMaven/Components/SideNav.razor.js
+++ MapMaven/Components/SideNav.razor.js
@@ -1,12 +1,18 @@
-const sideNavResizer = (function () {
-    const resizerHandle = document.querySelector(".resizer-handle");
-
+window.sideNavResizer = (function () {
+    const minSideNavWidth = 240;
     const maxSideNavWidth = 700;
 
-    const setDrawerWidth = throttle((width) => {
+    let sideNavWidth = localStorage.getItem("sideNavWidth") ?? 240;
+
+    setDrawerWidthNoThrottle(sideNavWidth);
+
+    const setDrawerWidth = throttle(setDrawerWidthNoThrottle, 30);
+
+    function setDrawerWidthNoThrottle(width) {
         // Set the side nav width to the current mouse position
+        sideNavWidth = width;
         document.documentElement.style.setProperty("--mud-drawer-width-left", `${width}px`);
-    }, 30);
+    }
 
     function startSideNavResize(event) {
         // Start tracking mouse movement
@@ -15,13 +21,15 @@ const sideNavResizer = (function () {
     }
 
     function onMouseMove(event) {
-        if (event.clientX < maxSideNavWidth)
+        if (event.clientX >= minSideNavWidth && event.clientX <= maxSideNavWidth)
             setDrawerWidth(event.clientX);
     }
 
     function stopSideNavResize() {
         document.removeEventListener("mouseup", stopSideNavResize);
         document.removeEventListener("mousemove", onMouseMove);
+
+        localStorage.setItem("sideNavWidth", sideNavWidth);
     }
 
     function throttle(mainFunction, delay) {
@@ -36,5 +44,12 @@ const sideNavResizer = (function () {
         }
     };
 
-    resizerHandle.addEventListener("mousedown", startSideNavResize);
+    function initialize() {
+        const resizerHandle = document.querySelector(".resizer-handle");
+        resizerHandle.addEventListener("mousedown", startSideNavResize);
+    }
+
+    return {
+        initialize: initialize
+    }
 })()
